4c0ba36e566c8af13bbe9d37107dac6873340b3d
Gurbaksh600 <gsathwal10@gmail.com>
2025-03-05 10:10:03 -0700

    Create march 5.py

---
+++ march 5.py
@@ -0,0 +1,7 @@
+myName = input("What's your name?: ")
+if myName == "Gurbaksh":
+ print("Welcome Dude!")
+ print("You're just the tallest  dude I've ever seen")
+else: 
+ print("Who on earth are you?!") 
+print("leave dude you werent invited")
